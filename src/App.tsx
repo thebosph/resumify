b6a5c9062cc9c0c58f74bff7dabe9f7dce71ef04
thebosph <haciibrahimsimsek@ogr.iu.edu.tr>
2022-10-15 15:12:30 +0300

    feat(indexcss): added roboto font to index.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <p className="text-4xl font-bold">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
